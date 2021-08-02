633b99d800fe3c6c64a52e0a612f3e9014f8f9a6
ShavarshP <papoyan101097@mail.ru>
2021-08-02 22:32:39 +0400

    create

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.get("/", cors(), (req, res) => {
   const customers = [
     { id: 1, firstName: "John", lastName: "Doe" },
     { id: 2, firstName: "Brad", lastName: "Traversy" },
-    { id: 3, firstName: "Mary", lastName: "Swanson" },
   ];
   res.send(customers);
 });
