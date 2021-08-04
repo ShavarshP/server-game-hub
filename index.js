b0bc5f94ce1d57f70535af44bc753894ec83d96c
ShavarshP <papoyan101097@mail.ru>
2021-08-04 14:32:51 +0400

    2 commit

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.get("/", cors(), (req, res) => {
-  const customers = [
-    { id: 1, firstName: "John", lastName: "Doe" },
-    { id: 2, firstName: "Brad", lastName: "Traversy" },
-  ];
-  res.send(customers);
+app.get("/", (req, res) => {
+  res.send("maladec");
 });
 
 const port = 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port);
