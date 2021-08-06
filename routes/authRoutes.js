05949ae9935ce2728ed1b2b5a20ed9340bd35ee4
ShavarshP <papoyan101097@mail.ru>
2021-08-06 11:16:47 +0400

    add: login.file

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -33,17 +33,6 @@ router.get("/test", async (req, res) => {
       password: "shavarsh222",
     };
 
-    const candidate = await User.findOne({ email });
-
-    if (candidate) {
-      return res.status(400).json({ message: "This user already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ email, userName, password: hashedPassword });
-
-    await user.save();
-
     res.status(201).json({ message: "User created" });
   } catch (e) {
     res.status(500).json({ message: "Something went wrong, please try again" });
---
+++ routes/login.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const router = Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, userName, password } = {
+      email: "shavarsh101097@gmmai.com",
+      userName: "Yamaklorm",
+      password: "shavarsh222",
+    };
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User is not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Invalid password, please try again" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, config.get("jwtSecret"), {
+      expiresIn: "1h",
+    });
+
+    res.json({ token, userId: user.id });
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong, please try again" });
+  }
+});
+
+module.exports = router;
