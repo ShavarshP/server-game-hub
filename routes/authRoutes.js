b02cfd45f9e912bbb59f35beefa26ab27db5ad5f
ShavarshP <papoyan101097@mail.ru>
2021-08-08 22:10:29 +0400

    add: middleware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -25,30 +25,4 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/test", async (req, res) => {
-  try {
-    const { email, userName, password } = {
-      email: "shavarsh101097@gmmai.com",
-      userName: "Yamaklorm",
-      password: "shavarsh222",
-    };
-    const candidate = await User.findOne({ email });
-
-    if (candidate) {
-      return res.status(400).json({ message: "This user already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const user = new User({ email, userName, password: hashedPassword });
-
-    await user.save();
-
-    res.status(201).json({ message: "User created" });
-  } catch (e) {
-    res
-      .status(500)
-      .json({ message: "Something went wrong, please try again Armin" });
-  }
-});
-
 module.exports = router;
