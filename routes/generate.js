f644ed10c7204c39a6bbd9b06fb91bf73a7448ea
ShavarshP <papoyan101097@mail.ru>
2021-08-14 00:12:45 +0400

    fix: generate

--- routes/generate.js
+++ routes/generate.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const auth = require("../middleware/auth.middleware");
+const Story = require("../models/story");
 const router = Router();
 
 router.post("/generate", auth, async (req, res) => {
   try {
     const data = req.body;
     console.log(data);
-
+    // const story = new Story({ userName, owner});
+    // await story.save();
     res.status(201).json({ link: "shash" });
   } catch (e) {
     res.status(500).json({ message: "Что-то пошло не так, попробуйте снова" });
