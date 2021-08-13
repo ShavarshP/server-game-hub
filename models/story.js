b90182ae67ad0329bea686ae2ea374af078fa5a1
ShavarshP <papoyan101097@mail.ru>
2021-08-14 00:08:33 +0400

    fix models str

--- models/story.js
+++ models/story.js
@@ -2,9 +2,9 @@ const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
   date: { type: Date, default: Date.now },
+  owner: { type: Types.ObjectId, ref: "User" },
   userName: { type: String, unique: true },
   clicks: { type: Number, default: 1000 },
-  record2048: { type: Number, default: 0 },
 });
 
 module.exports = model("Story", schema);
