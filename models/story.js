554610fb2da62b1105628e731bbb23b97e77d3fc
ShavarshP <papoyan101097@mail.ru>
2021-08-15 15:07:27 +0400

    fix: type-story

--- models/story.js
+++ models/story.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
   date: { type: Date, default: Date.now },
-  owner: { type: Types.ObjectId, ref: "User" },
+  owner: { type: String, ref: "User" },
   userName: { type: String, unique: true },
   clicks: { type: Number, default: 1000 },
 });
