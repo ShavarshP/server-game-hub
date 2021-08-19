18f78f827331d1c849bd57c727d21fe368bbb5a6
ShavarshP <papoyan101097@mail.ru>
2021-08-19 20:41:51 +0400

    change schema story

--- models/story.js
+++ models/story.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const schema = new Schema({
   date: { type: Date, default: Date.now },
   owner: { type: String, ref: "User" },
-  userName: { type: String, unique: false },
+  userName: { type: String, required: true },
   clicks: { type: Number, default: 1000 },
 });
 
