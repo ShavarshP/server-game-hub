9ef1e8a2b743861c60f46f6585e9fb8573a84c64
ShavarshP <papoyan101097@mail.ru>
2021-08-21 00:14:05 +0400

    connect: ws-io

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const config = require("config");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+const WSServer = require("express-ws")(app);
+// const aWss = WSServer.getWss();
 const cors = require("cors");
 
 app.use(bodyParser.json({ limit: "50mb" }));
@@ -20,8 +22,28 @@ app.use("/api", require("./routes/getDataList"));
 app.use("/api", require("./routes/login"));
 app.use("/api", require("./routes/generate"));
 // app.use("/api/results", require("./routes/gameResults"));
+let sms = "opaopaoppapa";
+app.ws("/io", (ws, req) => {
+  ws.send(sms);
+  ws.on("message", (msg) => {
+    sms = msg;
+  });
+});
 
 // app.use("/api/auth", require("./routes/authRoutes"));
+app.ws("/", (ws, req) => {
+  ws.on("message", (msg) => {
+    msg = JSON.parse(msg);
+    switch (msg.method) {
+      case "connection":
+        connectionHandler(ws, msg);
+        break;
+      case "draw":
+        broadcastConnection(ws, msg);
+        break;
+    }
+  });
+});
 
 const PORT = 5000;
 async function start() {
