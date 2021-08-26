bd98e602f612682129f29d3640d2f8d82b465a0a
ShavarshP <papoyan101097@mail.ru>
2021-08-26 23:47:00 +0400

    creat: game logic test

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const server = require("http").Server(app);
 
 const cors = require("cors");
+const getio = require("./liveServer/reversoContext");
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
@@ -29,36 +30,8 @@ app.use("/api", require("./routes/authRoutes"));
 app.use("/api", require("./routes/getDataList"));
 app.use("/api", require("./routes/login"));
 app.use("/api", require("./routes/generate"));
+getio(io);
 // app.use("/api/results", require("./routes/gameResults"));
-
-const rooms = new Map();
-
-io.on("connection", (socket) => {
-  socket.on("ROOM:JOIN", ({ roomId, userName }) => {
-    if (!rooms.get(roomId) || rooms.get(roomId).closed === null) {
-      socket.join(roomId);
-      if (!rooms.get(roomId)) {
-        rooms.set(roomId, { open: userName, closed: null });
-      } else {
-        rooms.set(roomId, { open: rooms.get(roomId).open, closed: userName });
-        socket.emit(
-          "ROOM:SET_USERS",
-          JSON.stringify({ open: rooms.get(roomId).open, closed: userName })
-        );
-      }
-      socket.broadcast
-        .to(roomId)
-        .emit("ROOM:SET_USERS", "JSON.stringify(rooms)");
-    } else {
-      socket.emit("ROOM:SET_USERS", "bum chaka-chaka");
-    }
-    // rooms[roomId] = [...rooms[roomId], socket.id];
-    // const users = [...rooms.get(roomId).get("users").values()];
-    // const users = userName;
-    // socket.to(roomId).broadcast.emit("ROOM:SET_USERS", users);
-    // socket.to(roomId).emit("ROOM:SET_USERS", users);
-  });
-});
 
 const PORT = 5000;
 async function start() {
--- liveServer/reversoContext.js
+++ liveServer/reversoContext.js
@@ -1,4 +1,53 @@
-const { Router } = require("express");
-const webSocketServer = require("websocket").server;
+const { cardsList } = require("../static/play_cards");
 
-module.exports = router;
+let cardArr = cardsList.filter((item) => item.index > 5);
+
+const getRandomCard = (index, acc = []) => {
+  if (index === 0 || cardArr == []) {
+    return acc;
+  }
+  const random = Math.floor(Math.random() * 36);
+  acc = [...acc, cardArr[random]];
+  cardArr = cardArr.filter((item, index) => index !== random);
+  return getRandomCard(index - 1, acc);
+};
+const getio = (io) => {
+  const rooms = new Map();
+  // console.log("maladec");
+  io.on("connection", (socket) => {
+    socket.on("ROOM:JOIN", ({ roomId, userName }) => {
+      try {
+        if (!rooms.get(roomId) || rooms.get(roomId).closed === null) {
+          socket.join(roomId);
+          if (!rooms.get(roomId)) {
+            rooms.set(roomId, {
+              open: { userName: userName, myCard: getRandomCard(6) },
+              closed: null,
+            });
+          } else {
+            rooms.set(roomId, {
+              open: rooms.get(roomId).open,
+              closed: { userName: userName, myCard: getRandomCard(6) },
+            });
+
+            socket.emit(
+              "ROOM:SET_USERS",
+              JSON.stringify(rooms.get(roomId).open)
+            );
+          }
+          socket.broadcast
+            .to(roomId)
+            .emit("ROOM:SET_USERS", JSON.stringify(rooms.get(roomId).closed));
+        } else {
+          socket.emit("ROOM:SET_USERS", "bum chaka-chaka");
+        }
+      } catch (error) {}
+      // rooms[roomId] = [...rooms[roomId], socket.id];
+      // const users = [...rooms.get(roomId).get("users").values()];
+      // const users = userName;
+      // socket.to(roomId).broadcast.emit("ROOM:SET_USERS", users);
+      // socket.to(roomId).emit("ROOM:SET_USERS", users);
+    });
+  });
+};
+module.exports = getio;
