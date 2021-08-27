e66ce6c81da398f2814063e7592308a69ca2421f
ShavarshP <papoyan101097@mail.ru>
2021-08-27 23:48:58 +0400

    creat: game logic test

--- liveServer/reversoContext.js
+++ liveServer/reversoContext.js
@@ -12,33 +12,30 @@ const getRandomCard = (index, acc = []) => {
   return getRandomCard(index - 1, acc);
 };
 const getio = (io) => {
-  const rooms = new Map();
+  const rooms = {};
   // console.log("maladec");
   io.on("connection", (socket) => {
     cardArr = cardsList.filter((item) => item.index > 4);
     socket.on("ROOM:JOIN", ({ roomId, userName }) => {
       try {
-        if (!rooms.get(roomId) || rooms.get(roomId).closed === null) {
+        if (!rooms.roomId || rooms.roomId.closed === null) {
           socket.join(roomId);
-          if (!rooms.get(roomId)) {
-            rooms.set(roomId, {
+          if (!rooms.roomI) {
+            rooms.roomId = {
               open: { userName: userName, myCard: getRandomCard(6) },
               closed: null,
-            });
+            };
           } else {
-            rooms.set(roomId, {
-              open: rooms.get(roomId).open,
+            rooms.roomId = {
+              open: rooms.roomId.open,
               closed: { userName: userName, myCard: getRandomCard(6) },
-            });
+            };
 
-            socket.emit(
-              "ROOM:SET_USERS",
-              JSON.stringify(rooms.get(roomId).open)
-            );
+            socket.emit("ROOM:SET_USERS", JSON.stringify(rooms.roomId.open));
           }
           socket.broadcast
             .to(roomId)
-            .emit("ROOM:SET_USERS", JSON.stringify(rooms.get(roomId).closed));
+            .emit("ROOM:SET_USERS", JSON.stringify(rooms.roomId.closed));
         } else {
           socket.emit("ROOM:SET_USERS", "bum chaka-chaka");
         }
